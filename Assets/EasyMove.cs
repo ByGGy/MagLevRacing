fc5bde9193378323468fe6cb53a68c08aa138ac2
ByGGy <ahcben@yahoo.fr>
2016-08-29 23:28:39 +0200

    Adding the Unity project with a few components

---
+++ Assets/EasyMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyMove : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+
+        float value = Input.GetAxis("Horizontal");
+        body.AddTorque(0, value*400, 0);
+
+        bool isThrustActive = Input.GetButton("Fire1");
+        if (isThrustActive)
+            body.AddForce(body.transform.forward*400);
+    }
+}
