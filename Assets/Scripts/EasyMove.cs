c6a010e994b4f545bce2d4d80d6df957900cd737
BM <benjamin.manderscheid@analogway.com>
2016-08-30 18:05:50 +0200

    Starting the thrust vectoring implementation (not compiling)

--- Assets/Scripts/EasyMove.cs
+++ Assets/Scripts/EasyMove.cs
@@ -2,7 +2,12 @@
 
 public class EasyMove : MonoBehaviour
 {
+#error Need a reference to following Empty GameObjects :
+        - Center of Mass
+        - Thruster Pivot
+
     private const float THRUST_MAX = 156000; //Newtons
+    private const float THRUST_VECTORING_ANGLE_MAX = 20;  //°
 
     private float nozzleAngle;
     private bool isThrustActive;
@@ -17,9 +22,13 @@ public class EasyMove : MonoBehaviour
 	{
 		Rigidbody rigidbody = GetComponent<Rigidbody>();
 
-        rigidbody.AddTorque(0, nozzleAngle * THRUST_MAX*2, 0);
+        //rigidbody.AddTorque(0, nozzleAngle * THRUST_MAX*2, 0);
 
-		if (isThrustActive)
-		    rigidbody.AddForce(rigidbody.transform.forward * THRUST_MAX);
+	    if (isThrustActive)
+	    {
+            Quaternion thrustAngle = Quaternion.AngleAxis(nozzleAngle * THRUST_VECTORING_MAX, rigidbody.transform.up);
+            Vector3 thrust = thrustAngle * rigidbody.transform.forward * THRUST_MAX;
+            rigidbody.AddForceAtPosition(thrust, rigidbody.transform.position - rigidbody.transform.forward*9);
+	    }
 	}
 }
