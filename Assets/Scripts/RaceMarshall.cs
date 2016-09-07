665f3182bf203bd9d99a334eb1790380cafb6aa0
BM <benjamin.manderscheid@analogway.com>
2016-09-07 17:24:43 +0200

    Started to use the checkpoint events to record the lap time

--- Assets/Scripts/RaceMarshall.cs
+++ Assets/Scripts/RaceMarshall.cs
@@ -2,14 +2,31 @@
 using System.Linq;
 using UnityEngine;
 
+public class LapIntermediateRecord
+{
+    public readonly uint CheckpointId;
+    public readonly float ElapsedTime;
+
+    public LapIntermediateRecord(uint checkpointId, float elapsedTime)
+    {
+        this.CheckpointId = checkpointId;
+        this.ElapsedTime = elapsedTime;
+    }
+}
+
 public class RaceMarshall : MonoBehaviour
 {
     private List<Checkpoint> checkpoints;
 
-	// Use this for initialization
+    private bool isRecordingLap;
+    private float elapsedTime;
+    private List<LapIntermediateRecord> intermediateRecords;
+
+    public IEnumerable<LapIntermediateRecord> IntermediateRecords { get { return this.intermediateRecords; } }
+
     private void Start()
 	{
-	    this.checkpoints = GetComponentsInChildren<Checkpoint>().ToList();
+	    this.checkpoints = GetComponentsInChildren<Checkpoint>().OrderBy(cp => cp.Id).ToList();
         this.checkpoints.ForEach(cp => cp.OnTargetDetected += OnCheckpointTriggered);
         Debug.Log(this.checkpoints.Count);
 	}
@@ -17,11 +34,40 @@ public class RaceMarshall : MonoBehaviour
     private void OnCheckpointTriggered(Checkpoint cp)
     {
         Debug.Log(string.Format("Checkpoint {0}", cp.Id));
+        if (cp.Id == this.checkpoints.First().Id)
+        {
+            if (!isRecordingLap)
+                StartRecordingLap(cp);
+            else
+                EndRecordingLap(cp);
+        }
+        else
+            RecordLap(cp)
     }
 
-	// Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-	
-	}
+        this.elapsedTime += fixedDeltaTime;
+    }
+
+    private void StartRecordingLap(Checkpoint cp)
+    {
+        this.isRecordingLap = true;
+        this.elapsedTime = 0;
+        this.intermediateRecords = new List<LapIntermediateRecord>();
+        this.intermediateRecords.Add(new LapIntermediateRecord(cp.Id, this.elapsedTime));
+    }
+
+    private void RecordLap(Checkpoint cp)
+    {
+        if (cp.Id == this.intermediateRecords.Last().CheckpointId + 1)
+            this.intermediateRecords.Add(new LapIntermediateRecord(cp.Id, this.elapsedTime));
+    }
+
+    private void EndRecordingLap(Checkpoint cp)
+    {
+        this.isRecordingLap = false;
+        this.intermediateRecords.Add(new LapIntermediateRecord(cp.Id, this.elapsedTime));
+        Debug.Log(string.Format("Lap Time = {0} ms", this.elapsedTime));
+    }
 }
