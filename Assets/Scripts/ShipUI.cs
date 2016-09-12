3ee997fdd376dc7548a211a47b3fea926dfb5425
BM <benjamin.manderscheid@analogway.com>
2016-09-12 13:59:36 +0200

    Improved UI (look & display of gaps with best lap)

--- Assets/Scripts/ShipUI.cs
+++ Assets/Scripts/ShipUI.cs
@@ -1,14 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ShipUI : MonoBehaviour
 {
     public Transform Source;
 
+    private Text shipText;
+
+    private void Start()
+    {
+        this.shipText = GetComponentsInChildren<Text>().Where(c => string.Equals(c.name, "ShipText")).FirstOrDefault();
+    }
+
 	// Update is called once per frame
     private void Update()
 	{
         int velocityInKmPerHour = (int)(this.Source.GetComponent<Rigidbody>().velocity.magnitude * 3600 / 1000.0f);
-	    GetComponent<Text>().text = string.Format("Speed : {0}Km/h", velocityInKmPerHour);
+        this.shipText.text = string.Format("Speed : {0}Km/h", velocityInKmPerHour);
 	}
 }
